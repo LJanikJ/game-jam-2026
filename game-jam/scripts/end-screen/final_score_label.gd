0d06556f67680e20e19803c31d35ce0e9ab99e0c
Royce-33 <adams.riley@rogers.com>
2026-08-27 19:13:32 -0400

    Add scoring to gameplay & display on end screen

---
+++ game-jam/scripts/end-screen/final_score_label.gd
@@ -0,0 +1,34 @@
+extends RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_end_screen_final_score(score: int) -> void:
+	if (score >= 0):
+		self.text = """Shift Over!
+
+You made a profit of %d gold pieces.
+
+[wave]testing[/wave]
+[shake]testing[/shake]
+[fade]testing[/fade]
+[rainbow]testing[/rainbow]""" % [score]
+	
+	else:
+		self.text = """Shift Over!
+
+You're Fired!
+You lost the shop %d gold pieces.
+
+[wave]testing[/wave]
+[shake]testing[/shake]
+[fade]testing[/fade]
+[rainbow]testing[/rainbow]""" % [score * -1]
