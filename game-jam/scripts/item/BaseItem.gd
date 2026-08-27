ae3df8400da401615b173fcd77e8a8926ae0cb7e
ljanikj <lukejj@me.com>
2026-08-27 18:41:20 -0400

    redid item structure

--- game-jam/scripts/item/BaseItem.gd
+++ game-jam/scripts/item/BaseItem.gd
@@ -4,11 +4,8 @@ var cursed: bool
 var lying: bool
 @export var item_name: String
 
-# common = 1, uncommon = 2, rare = 5, legendary = 10
-var rarity: int
-
-# quality is an int ranging from 0 to X
-var quality: int
+# value is an int ranging from -X to Y
+@export var value: int
 
 # decides what page of the book governs it
 enum ItemType {ORB, WAND, WEAPON, MISC}
@@ -36,17 +33,17 @@ func load_item(index: int) -> void:
 		# set parameters
 		cursed = json_entry["cursed"]
 		lying = json_entry["lying"]
-		quality = json_entry["quality"]
+		value = json_entry["value"]
 		
 		# set multiplier based on rarity
-		if json_entry["rarity"] == "common":
-			rarity = 1
-		elif json_entry["rarity"] == "uncommon":
-			rarity = 2
-		elif json_entry["rarity"] == "rare":
-			rarity = 5
-		elif json_entry["rarity"] == "legendary":
-			rarity = 10
+		#if json_entry["rarity"] == "common":
+			#rarity = 1
+		#elif json_entry["rarity"] == "uncommon":
+			#rarity = 2
+		#elif json_entry["rarity"] == "rare":
+			#rarity = 5
+		#elif json_entry["rarity"] == "legendary":
+			#rarity = 10
 		
 		if json_entry["type"] == "weapon":
 			type = ItemType.WEAPON
@@ -62,21 +59,3 @@ func load_item(index: int) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
-# placeholder value calc
-func get_value() -> int:
-	var baseValue = 0
-	
-	if cursed or lying:
-		return 0
-	
-	if type == ItemType.ORB:
-		baseValue = 100
-	elif type == ItemType.WAND:
-		baseValue = 100
-	elif type == ItemType.WEAPON:
-		baseValue = 100
-	elif type == ItemType.MISC:
-		baseValue = 100
-		
-	return baseValue * rarity * quality
