4b3d0d2420f4fb49a3022edce80a1412a5af790c
Royce-33 <adams.riley@rogers.com>
2026-08-26 21:59:29 -0400

    Add settings menu for volume controls, channels for master, BGM, SFX

--- game-jam/scenes/desk/slide_audio.gd
+++ game-jam/scenes/desk/slide_audio.gd
@@ -21,4 +21,5 @@ func _on_animation_player_animation_started(anim_name: StringName) -> void:
 	if anim_name == "slide_in":
 		var index = randi_range(0,3)
 		stream = slide[index]
+		get_tree().call_group("SFX", "set_volume_db", -20.0)
 		play()
---
+++ game-jam/scripts/options-screen/options_screen.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var master_idx = AudioServer.get_bus_index("Master")
+var bgm_idx = AudioServer.get_bus_index("BGM")
+var sfx_idx = AudioServer.get_bus_index("SFX")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/MasterSlider.value = db_to_linear(AudioServer.get_bus_volume_db(master_idx))
+	$VBoxContainer/BGMSlider.value = db_to_linear(AudioServer.get_bus_volume_db(bgm_idx))
+	$VBoxContainer/SFXSlider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_idx))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_master_slider_drag_ended(value_changed: bool) -> void:
+	AudioServer.set_bus_volume_db(master_idx, linear_to_db($VBoxContainer/MasterSlider.value))
+
+
+func _on_bgm_slider_drag_ended(value_changed: bool) -> void:
+	AudioServer.set_bus_volume_db(bgm_idx, linear_to_db($VBoxContainer/BGMSlider.value))
+
+
+func _on_sfx_slider_drag_ended(value_changed: bool) -> void:
+	AudioServer.set_bus_volume_db(sfx_idx, linear_to_db($VBoxContainer/SFXSlider.value))
+
+
+func _on_return_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/title-screen/title-screen.tscn")
