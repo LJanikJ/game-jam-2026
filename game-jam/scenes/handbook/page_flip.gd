7f669bc544465ba3d100167343815dbe47b848f5
ljanikj <lukejj@me.com>
2026-08-27 15:55:49 -0400

    Tweak flip sound

--- game-jam/scenes/handbook/page_flip.gd
+++ game-jam/scenes/handbook/page_flip.gd
@@ -1,11 +1,10 @@
 extends AudioStreamPlayer2D
 
 var open = preload("res://resources/sound_effects/book/open.wav")
-var flip = preload("res://resources/sound_effects/book/open.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	stream = flip
+	stream = open
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,10 +13,10 @@ func _process(_delta: float) -> void:
 
 
 func _on_handbook_button_pressed() -> void:
-	stream = open
+	volume_db = 0.0
 	play()
 
 
 func _on_page_sprite_texture_changed() -> void:
-	stream = flip
+	volume_db = -5.0
 	play()
