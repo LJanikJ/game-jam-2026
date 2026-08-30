16ce704b2dea20bfc91808ec08d47a3505094e33
ljanikj <lukejj@me.com>
2026-08-29 20:02:36 -0400

    Added some SFX, fixed the tutorial and the audiobus

--- game-jam/scripts/music/sfx_player.gd
+++ game-jam/scripts/music/sfx_player.gd
@@ -3,9 +3,14 @@ extends AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
+
+
+func _on_tabletop_view_sfx(sfx: String) -> void:
+	stream = load("res://resources/sound_effects/item/"+sfx+".wav")
+	play()
